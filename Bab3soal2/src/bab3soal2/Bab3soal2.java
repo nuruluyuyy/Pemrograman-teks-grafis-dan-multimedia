81ba28e3e2506345a05821d74a4f72d4bad24f8a
nuruluyuyy <nurulyuyy111@gmail.com>
2024-08-12 18:58:58 +0700

    Add files via upload

---
+++ Bab3soal2/src/bab3soal2/Bab3soal2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bab3soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NIM = 2110117000;
+        String NamaMahasiswa = "Andika Jaya Utama";
+        String Kelas = "01SIFE020";
+        double Usia = 19.5;
+        double Tinggi = 1.72;
+        
+        System.out.println("NIM : " + NIM);
+        System.out.println("NamaMahasiswa : " +  NamaMahasiswa);
+        System.out.println("Kelas : " + Kelas);
+        System.out.println("Usia : " + Usia);
+        System.out.println("Tinggi : " + Tinggi);
+    }
+    
+}
