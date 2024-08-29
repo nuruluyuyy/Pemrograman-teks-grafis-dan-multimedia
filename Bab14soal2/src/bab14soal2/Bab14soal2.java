41d80f9947370f769ef76580204310e87845ee6c
nuruluyuyy <nurulyuyy111@gmail.com>
2024-08-29 15:38:39 +0700

    Add files via upload

---
+++ Bab14soal2/src/bab14soal2/Bab14soal2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab14soal2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bab14soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       Burung burung = new Burung();
+       
+       burung.suara();
+       
+       burung.terbang();
+        
+    }
+    
+}
---
+++ Bab14soal2/src/bab14soal2/Burung.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab14soal2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Burung {
+    
+    void suara() {
+        System.out.println("Burung Berkicau");
+    }
+    void terbang() {
+        System.out.println("Burung Terbang di Udara");
+    }
+    
+
+    
+}
